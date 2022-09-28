1f972e62a9f669cdb32d2f0dd174958eaea3ad0f
ZephyrClint <it20148768@my.sliit.lk>
2022-09-28 17:11:29 +0530

    Set up navigation updating course material.

--- frontend/src/components/teacher/course material/CourseMaterialTeacherView.jsx
+++ frontend/src/components/teacher/course material/CourseMaterialTeacherView.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {  useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import axios from "axios"
 import Swal from "sweetalert2";
 
@@ -11,6 +11,7 @@ function CourseMaterialTeacherView() {
     const [errorMsg, setErrorMsg] = useState('');
     const[deleteCount, setDeleteCount] = useState(0);
     const [searchKeyword, setSearchKeyword] = useState("");
+    const navigate = useNavigate();
 
     let filename = ""
     let size = 0
@@ -71,6 +72,7 @@ function CourseMaterialTeacherView() {
                         <td> { filename } </td>
                         <td>
                             <button className='btn btn-outline-primary'
+                                onClick={()=>navigate(`/teachers/updateCourseMaterial/${material._id}`)}
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
